3179d774c2bb08898bc5389746f45088a020c711
Eli <elize.san10@gmail.com>
2021-03-11 11:52:18 -0600

    wip

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ if (process.env.NODE_ENV === "development") {
   const cors = require("cors");
   server.use(cors());
 }
+
+server.use("*", (req, res) => {
+  res.send("<h1>Success</h1>");
+});
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
